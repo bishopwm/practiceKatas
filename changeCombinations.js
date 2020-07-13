4a0a2184b162a6d2db08d8ff089e22b8199ec2bd
bishopwm <bishopwm@g.cofc.edu>
2020-07-13 13:35:02 -0400

    code wars kata

--- changeCombinations.js
+++ changeCombinations.js
@@ -11,3 +11,7 @@
 //   countChange(4, [1,2]) // => 3
 //   countChange(10, [5,2,3]) // => 4
 //   countChange(11, [5,7]) //  => 0
+
+var countChange = function(money, coins) {
+    // your implementation here
+  }
